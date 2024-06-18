ec08a0b6cceb6e3a7874706cbc78b4241f2c01b4
Vaibhav17031996 <vaibhavpatwal3@gmail.com>
2024-06-18 19:58:12 +0530

    likeOrUpdateBlog half done

--- src/controllers/blog.controller.js
+++ src/controllers/blog.controller.js
@@ -118,7 +118,74 @@ async function updateBlog(req, res) {
 }
 
 async function deleteBlog(req, res) {
-  res.send("working");
+  // delete the blog
+  // remove the tags associated with the blogId
+  const blogId = req.blog._id;
+  try {
+    const deletedBlog = await Blog.findByIdAndUpdate(blogId);
+    deletedBlog.tag.forEach(async (tagEntry) => {
+      const tagDocument = await Tag.findOne({ categoryName: tagEntry });
+      if (tagDocument) {
+        tagDocument.category.pull(blogId);
+        await tagDocument.save();
+      }
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      status: false,
+      message: "Could not delete the blog",
+      error: err.message,
+    });
+  }
 }
 
-module.exports = { createBlog, updateBlog, deleteBlog };
+async function likeOrUpdateBlog(req, res) {
+  const userId = req.user._id;
+  const blogId = req.params.blogId;
+  try {
+    const blog = await Blog.findById(blogId);
+    if (!blog) {
+      return res.status(400).json({
+        status: false,
+        message: "No blog found with this blog id",
+        error: "No blog found with this blog id",
+      });
+    }
+
+    if (blog.votedBy && blog.votedBy.length > 0) {
+      const isUserPresent = blog.votedBy.find((entry) => entry == userId);
+      if (isUserPresent) {
+        return res.status(400).json({
+          status: false,
+          message: "User has already voted before",
+        });
+      }
+    }
+
+    if (req.params.liking) {
+      blog.upVote = blog.upvote + 1;
+      blog.votedBy = [];
+      blog.votedBy = blog.votedBy.push(userId);
+      await blog.save();
+    } else {
+      blog.downVote = blog.downvote + 1;
+      blog.votedBy = [];
+      blog.votedBy = blog.votedBy.push(userId);
+      await blog.save();
+    }
+    return res.status(200).json({
+      status: true,
+      message: "Liking successfully done",
+    });
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).json({
+      status: false,
+      message: "Could not delete the blog",
+      error: err.message,
+    });
+  }
+}
+
+module.exports = { createBlog, updateBlog, deleteBlog, likeOrUpdateBlog };
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,21 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createBlog,
-  updateBlog,
-  deleteBlog,
-} = require("../controllers/blog.controller");
-const {
-  verifyAndFetchUser,
-} = require("../middlewares/verifyAndFetchUser.middleware");
-const {
-  verifyBlogCreator,
-} = require("../middlewares/verifyBlogCreator.middleware");
+const { createBlog, updateBlog, deleteBlog, likeOrUpdateBlog } = require("../controllers/blog.controller");
+const { verifyAndFetchUser } = require("../middlewares/verifyAndFetchUser.middleware");
+const { verifyBlogCreator } = require("../middlewares/verifyBlogCreator.middleware");
 
 router.route("/").post(verifyAndFetchUser, createBlog);
 router.route("/:blogId").put(verifyAndFetchUser, verifyBlogCreator, updateBlog);
-router
-  .route("/:blogId")
-  .delete(verifyAndFetchUser, verifyBlogCreator, deleteBlog);
+router.route("/:blogId").delete(verifyAndFetchUser, verifyBlogCreator, deleteBlog);
+router.route("/:blogId/upvote-downvote/:liking").post(verifyAndFetchUser, likeOrUpdateBlog);
 
 module.exports = router;
